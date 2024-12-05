c2a4e35d90284cd623eebca0e62bfb20ff4997a1
Raagul <IT22130020@my.sliit.lk>
2024-12-05 21:52:27 +0530

    Home.js feature

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,60 +4,63 @@ import { getProducts } from "../actions/productActions";
 import Loader from "./layouts/Loader";
 import MetaData from "./layouts/MetaData";
 import Product from "./product/Product";
-import  {toast} from 'react-toastify';
-import Pagination from 'react-js-pagination';
+import { toast } from "react-toastify";
+import Pagination from "react-js-pagination";
 
-export  default function Home(){
+
+export default function Home() {
     const dispatch = useDispatch();
-    const {products, loading, error, productsCount, resPerPage} =    useSelector((state) => state.productsState)
+    const { products, loading, error, productsCount, resPerPage } = useSelector(
+        (state) => state.productsState
+    );
     const [currentPage, setCurrentPage] = useState(1);
- 
-    const setCurrentPageNo = (pageNo) =>{
 
-        setCurrentPage(pageNo)
-       
-    }
+    const setCurrentPageNo = (pageNo) => {
+        setCurrentPage(pageNo);
+    };
 
-    useEffect(()=>{
-        if(error) {
-            return toast.error(error,{
-                position: toast.POSITION.BOTTOM_CENTER
-            })
+    useEffect(() => {
+        if (error) {
+            return toast.error(error, {
+                position: toast.POSITION.BOTTOM_CENTER,
+            });
         }
-        dispatch(getProducts(null, null, null, null, currentPage)) 
-    }, [error, dispatch, currentPage])
-
+        dispatch(getProducts(null, null, null, null, currentPage));
+    }, [error, dispatch, currentPage]);
 
     return (
         <Fragment>
-            {loading ? <Loader/>:
+            {loading ? (
+                <Loader />
+            ) : (
                 <Fragment>
-                    <MetaData title={'Buy Best Products'} />
+                    <MetaData title={"Buy Best Products"} />
                     <h1 id="products_heading">Latest Products</h1>
-                    <section id="products" className="container mt-5">
+                    <section id="products" className="container mt-5 scrollable-container">
                         <div className="row">
-                            { products && products.map(product => (
-                                <Product col={3} key={product._id}  product={product}/>
-                            ))}
-                        
+                            {products &&
+                                products.map((product) => (
+                                    <Product col={3} key={product._id} product={product} />
+                                ))}
                         </div>
                     </section>
-                    {productsCount > 0 && productsCount > resPerPage?
-                    <div className="d-flex justify-content-center mt-5">
-                           <Pagination 
+                    {productsCount > 0 && productsCount > resPerPage ? (
+                        <div className="d-flex justify-content-center mt-5">
+                            <Pagination
                                 activePage={currentPage}
                                 onChange={setCurrentPageNo}
                                 totalItemsCount={productsCount}
                                 itemsCountPerPage={resPerPage}
-                                nextPageText={'Next'}
-                                firstPageText={'First'}
-                                lastPageText={'Last'}
-                                itemClass={'page-item'}
-                                linkClass={'page-link'}
-                           />     
-                    </div> : null }
+                                nextPageText={"Next"}
+                                firstPageText={"First"}
+                                lastPageText={"Last"}
+                                itemClass={"page-item"}
+                                linkClass={"page-link"}
+                            />
+                        </div>
+                    ) : null}
                 </Fragment>
-           }
+            )}
         </Fragment>
-    )
+    );
 }
